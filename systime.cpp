4bfc3d322b5673816641dc46ec68750b57a63048
JosephASG <alexis200123@outlook.es>
2022-12-09 05:36:41 -0500

    runtime en c++

--- systime.cpp
+++ systime.cpp
@@ -1,7 +1,3 @@
-// Calcular tiempo de ejecución de una operación o función 
-// Autor: Joseph Santamaria
-// Fecha: 08/12/2022
-
 #include <bits/stdc++.h>
 #include <algorithm>
 #include <iostream>
@@ -11,15 +7,12 @@ using namespace std;
 
 long obtenerTiempo(){
     struct timeval inicio;
-    // gettimeofdat devuelve el tiempo del reloj de la computadora
     gettimeofday(&inicio, NULL);
-    // Retornamos el tiempo en segundo multiplicado por un millon que será 00:00:00 + el tiempo en microsegundos
     return inicio.tv_sec*1000000+inicio.tv_usec;
 }
 
 int main(){
     long inicio = obtenerTiempo(); // instancia inicial
-    // incio de operación random
     double tiempoEnSegundos;
     long tiempoEnMicrosegundos;
     int arr[] = {5,7,3,6,1,9,8,4,2};
@@ -28,14 +21,10 @@ int main(){
     for(int i=0; i < sizeArray; i++){
         cout<<arr[i]<<' ';
     }
-    // fin de operación random
     long final = obtenerTiempo(); // instancia final
-    // restamos el tiempo total final menos el tiempo de incio 
     tiempoEnMicrosegundos = final - inicio;
-    // transformamos a segundos
     tiempoEnSegundos = tiempoEnMicrosegundos * pow(10, -6);
     cout<<endl<<endl;
-    // impresion del tiempo en microsegundos y segundos
     cout<<"El tiempo de ejecucion en microsegundos es: "<<tiempoEnMicrosegundos<<endl<<endl;
     cout<<"EL tiempo de ejecucion en segundos es: "<<tiempoEnSegundos<<endl<<endl;
     return 0;
